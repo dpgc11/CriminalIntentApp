11f30872c56b63010a2423f508632679632872e2
dpgc11 <iamyogeshtelang@gmail.com>
2017-01-23 00:17:25 +0530

    added layout for landscape

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,10 +11,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -22,6 +26,22 @@ public class Crime {
 
     public String getTitle() {
         return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public void setTitle(String title) {
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -8,7 +8,13 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Yogesh on 21-01-2017.
@@ -18,11 +24,19 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+    private SimpleDateFormat mSimpleDateFormat;
+    private Date mCrimeDate;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+
+        mSimpleDateFormat = new SimpleDateFormat("EEEE, MMM d, yyyy");
+        mCrimeDate = mCrime.getDate();
+
     }
 
     @Nullable
@@ -45,6 +59,19 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable editable) {
 
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mSimpleDateFormat.format(mCrimeDate));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                // Set the crime's solved property
+                mCrime.setSolved(b);
             }
         });
 
