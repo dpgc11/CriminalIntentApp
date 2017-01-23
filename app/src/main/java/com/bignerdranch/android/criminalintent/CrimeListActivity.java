5343580962692df8e34f410409a35d72aad63ed5
dpgc11 <iamyogeshtelang@gmail.com>
2017-01-24 01:24:04 +0530

    add simpleFragmentActivity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,13 +2,13 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
+/**
+ * Created by Yogesh on 24-01-2017.
+ */
 
-public class CrimeActivity extends SingleFragmentActivity {
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
-
-
 }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Yogesh on 24-01-2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
